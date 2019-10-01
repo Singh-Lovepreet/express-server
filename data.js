d9a7b262dfca1bc2431b79ca98b92611a028d259
Lovepreet <lovepreet.singh@medtrail.in>
2019-10-01 16:51:19 +0530

    changes done

--- data.js
+++ data.js
@@ -15,5 +15,39 @@ const books=[{
     title:"forth book"
     ,author:"4-person"
 }];
+const books=[{
+    title:"first book"
+    ,author:"1-person"
+
+},{
+    title:"second book"
+    ,author:"2-person"
+
+},{
+    title:"third book"
+    ,author:"3-person"
+
+},
+    {
+        title:"forth book"
+        ,author:"4-person"
+    }];
+const books=[{
+    title:"first book"
+    ,author:"1-person"
+
+},{
+    title:"second book"
+    ,author:"2-person"
+
+},{
+    title:"third book"
+    ,author:"3-person"
+
+},
+    {
+        title:"forth book"
+        ,author:"4-person"
+    }];
 
 module.exports=books
